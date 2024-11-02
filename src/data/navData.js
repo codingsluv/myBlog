a0c11128e6773febdee72a309e3925d8d72ed138
MuhammadNur <delusionboys974@gmail.com>
2024-11-03 00:48:06 +0700

    add components

---
+++ src/data/navData.js
@@ -0,0 +1,14 @@
+const navData = [
+    {
+        name: "About",
+        path: "/about",
+        // icon: "home"
+    },
+    {
+        name: "Blog",
+        path: "/blog",
+        // icon: "info"
+    },
+]
+
+export default navData
