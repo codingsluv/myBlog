c101c9d6df80ed68ce2da4c852015ccc1f0456c1
MuhammadNur <delusionboys974@gmail.com>
2024-11-03 16:48:02 +0700

    set:deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,11 @@ import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   integrations: [icon(), mdx()],
+  site: 'https://astronaut.github.io',
+  base: 'https://github.com/codingsluv.github.io.',
+  output: 'dist',
+  adapter: staticAdapter(),
+  devOptions: {
+    port: 3000,
+  },
 });
