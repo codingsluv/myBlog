e611c80102b5f98fc009494223f9cea28e128049
MuhammadNur <delusionboys974@gmail.com>
2024-11-03 16:36:11 +0700

    style(content blog): capek

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import icon from "astro-icon";
 
 import mdx from "@astrojs/mdx";
 
-import image from "@astrojs/image";
 
 export default defineConfig({
-  integrations: [icon(), mdx(), image({
-    serviceEntryPoint: '@astrojs/image/sharp',
-  })],
+  integrations: [icon(), mdx()],
 });
--- src/data/posts.js
+++ src/data/posts.js
@@ -1,14 +1,30 @@
 export const posts = [
     {
-        title: "A Post about Important Items Of Life",
-        date: "2022-11-20",
-        description: "Have you ever wondered what the most important items of life are? Well, wonder no more!",
+        title: "test post 1",
+        author: "Muhammad Nur",
+        date: "2024-01-01",
+        description: "Have you ever wondered",
         link: "/blog/post-1",
     },
     {
-        title: "test post",
-        date: "2022-11-20",
-        description: "test post",
+        title: "test post 2",
+        author: "Muhammad Nur",
+        date: "2024-01-02",
+        description: "lorem ipsum",
         link: "/blog/post-2",
-    }
+    },
+    {
+        title: "test post 3",
+        author: "Muhammad Nur",
+        date: "2024-01-03",
+        description: "lorem ipsum",
+        link: "/blog/post-3",
+    },
+    {
+        title: "test post 4",
+        author: "Muhammad Nur",
+        date: "2024-01-04",
+        description: "lorem ipsum",
+        link: "/blog/post-4",
+    },
 ]
