12abb565f921efbbc77e1719a145690c5890fa94
MuhammadNur <delusionboys974@gmail.com>
2024-11-03 13:21:15 +0700

    wip: content blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import icon from "astro-icon";
 
 import mdx from "@astrojs/mdx";
 
+import image from "@astrojs/image";
+
 export default defineConfig({
-  integrations: [icon(), mdx()],
+  integrations: [icon(), mdx(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+  })],
 });
--- src/pages/blog/post-1.md
+++ src/pages/blog/post-1.md
@@ -1,4 +1,5 @@
 ---
+layout: ../../layouts/MainLayout.astro
 title: A Post about Important Items Of Life
 date: 2022-11-20
 author: Darnell McClure
