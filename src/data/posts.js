c2e76a08acc725b089cd028d22f6368a6ac7c2c9
MuhammadNur <delusionboys974@gmail.com>
2024-11-03 12:05:06 +0700

    add: card blog post and data sample

---
+++ src/data/posts.js
@@ -0,0 +1,14 @@
+export const posts = [
+    {
+        title: "A Post about Important Items Of Life",
+        date: "2022-11-20",
+        description: "Have you ever wondered what the most important items of life are? Well, wonder no more!",
+        link: "/blog/post-1",
+    },
+    {
+        title: "test post",
+        date: "2022-11-20",
+        description: "test post",
+        link: "/blog/post-2",
+    }
+]
